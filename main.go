21821a787a20a12827ec4fd54be08a11f8329633
Nely <nelymurtazina@gmail.com>
2026-05-19 22:30:36 +0300

    feat:task4/lesson1

--- main.go
+++ main.go
@@ -1,115 +1,44 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
-type Device interface {
-	UpdateOs(version string) error
-	GetInfo() string
-}
-type Smartphone struct {
-	OSVersion string
-	Model     string
-}
-type Laptop struct {
-	OSVersion string
-	Model     string
-}
-type Smartwatch struct {
-	OSVersion string
-	Model     string
+type MyStruct struct {
+	MyInt int
 }
 
-var (
-	ErrUnsupported = errors.New("обновление недоступно")
-)
-
-func (phone *Smartphone) UpdateOS(version string) error {
-	num, err := strconv.Atoi(version)
-	if err != nil {
-		fmt.Println("Error", err)
-		return err
-	}
-	if num <= 12.0{
-		fmt.Println(ErrUnsupported)
-		return ErrUnsupported
-	}
-	phone.OSVersion = version
-	fmt.Println("Model: " + phone.Model +"," + " OC: " + phone.OSVersion)
-	return nil
+func func1() MyStruct {
+	return MyStruct{MyInt: 1}
 }
 
-func (phone *Smartphone) GetInfo() string {
-	str := "Model: " + phone.Model +"," + " OC: " + phone.OSVersion
-	fmt.Println(str)
-	return str 
+func func2() *MyStruct {
+	return &MyStruct{}
 }
 
-func (lap *Laptop) UpdateOS(version string) error{
-	str := "Windows"
-	new := strings.Split(version, " ")
-	if str == new[0]{
-		lap.OSVersion = new[1]
-		fmt.Println(version)
-	} 
-	if str != new[0]{
-		fmt.Println(ErrUnsupported)
-		return ErrUnsupported
-	}
-	return nil
+func func3(s *MyStruct) {
+	s.MyInt = 333
 }
 
-func (lap *Laptop) GetInfo() string {
-	str := "Model: " + lap.Model +"," + " OC: " + lap.OSVersion
-	fmt.Println(str)
-	return str 
+func func4(s MyStruct) {
+	s.MyInt = 923
 }
 
-func (whatch *Smartwatch) GetInfo() string {
-	str := "Model: " + whatch.Model +"," + " OC: " + whatch.OSVersion
-	fmt.Println(str)
-	return str 
-}
-
-func (whatch *Smartwatch) UpdateOS(version string) error{
-	new := strings.Split(version, ".")
-	if len(new) > 5 {
-		fmt.Println(ErrUnsupported)
-		return ErrUnsupported
-	}
-	whatch.OSVersion = version
-	fmt.Println("Часы : ", whatch.Model, ",",  "Версия: ", whatch.OSVersion)
+func func5() *MyStruct {
 	return nil
 }
 
 func main() {
-	tel := Smartphone{
-		OSVersion: "11.6",
-		Model: "Iphone",
-	}
-	tel.GetInfo()
-	tel.UpdateOS("5")
+	ms1 := func1()
+	fmt.Println(ms1.MyInt) //1
 
-	HP:= Laptop{
-		OSVersion: "7",
-		Model: "Windows",
-	}
-	Mac:= Laptop{
-		OSVersion: "17",
-		Model: "Mac",
-	}
-	HP.UpdateOS("Windows 11")
-	Mac.UpdateOS("Mac 11")
+	ms2 := func2()
+	fmt.Println(ms2.MyInt)//0
 
-	Apple := Smartwatch{
-		OSVersion: "3.1.0",
-		Model: "Apple",
-	}
+	func3(ms2)
+	fmt.Println(ms2.MyInt)//333
 
-	Apple.GetInfo()
-	Apple.UpdateOS("5.1.5.3")
+	func4(ms1)
+	fmt.Println(ms1.MyInt)//1
+
+	ms5 := func5()
+	fmt.Println(ms5.MyInt)//последнее значение?
 }
