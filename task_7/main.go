dad11f6490e6b35036b43a40dc42c79592370d9a
Nely <nelymurtazina@gmail.com>
2026-05-20 20:35:46 +0300

    feat: task map 7

---
+++ task_7/main.go
@@ -0,0 +1,102 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var numbers []*int
+// 	for _, value := range []int{10, 20, 30, 40} {
+// 		//value - одна и та же переменная.
+// 		numbers = append(numbers, &value)
+// 	}
+// 	for _, number := range numbers {
+// 		fmt.Println("d", *number) // 10,20,30,40
+// 	}
+// }
+
+// //### 2.
+// package main
+
+// import (
+// 	"fmt"
+// 	"strings"
+// )
+
+// func chengeSlice(arr []string) {
+// 	arr[0] = "Goodbye"
+// }
+
+// func appendSomeData(arr []string) {
+// 	arr = append(arr, "!")
+// }
+
+// func main() {
+// 	someSlice := []string{"Hello", "World"}
+// 	chengeSlice(someSlice)
+// 	appendSomeData(someSlice)
+// 	fmt.Println(strings.Join(someSlice, "")) //GoodbyeWorld
+// }
+// ```
+// ----
+// ### 3.
+// ```
+// package main
+
+// import "fmt"
+
+//всегда ли передается копия заголовков? Путаюсь
+//len=2, cap=3
+// func test(testSlice []string) {
+// 	testSlice = append(testSlice, "Пока")
+// }
+// func main() {
+// 	testSlice := make([]string, 0, 3)
+// 	testSlice = append(testSlice, "Привет")
+// 	testSlice = append(testSlice, "Привет")
+// 	test(testSlice)
+// 	fmt.Println(testSlice) //[Привет Привет]
+// }
+// ```
+// ----
+// ### 4.
+// ```
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	first := []int{10, 20, 30, 40}
+// 	second := make([]*int, len(first))
+// 	for i, v := range first {
+// 		second[i] = &v
+// 	}
+// 	fmt.Println(*second[0], *second[1]) //10 20
+// }
+// ```
+// ----
+// ### 5.
+// ```
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	slice := make([]string, 3, 4)
+// 	fmt.Println(slice) //[]
+
+// 	appendSlice(slice)
+// 	fmt.Println(slice)//[]
+
+// 	mutareSlice(slice)
+// 	fmt.Println(slice)//[vasya]
+// }
+
+// func appendSlice(slice []string) {
+// 	slice = append(slice, "privet")
+// }
+// func mutareSlice(slice []string) {
+// 	slice[0] = "vasya"
+// }
