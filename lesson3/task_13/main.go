af35a366906b3dba103c629f5fbe4c284a88c061
Nely <nelymurtazina@gmail.com>
2026-06-02 17:42:13 +0300

    feat:update task_13

--- lesson3/task_13/main.go
+++ lesson3/task_13/main.go
@@ -3,35 +3,53 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
-type Restaurant struct {
-	tables int        // количество свободных столиков
-	mu     sync.Mutex // мьютекс для защиты tables
-	cond   *sync.Cond // условная переменная
+type Restaurant struct{
+	table []int
+	maxTables int
+	mu sync.Mutex
+	cond  *sync.Cond
 }
 
-func (r *Restaurant) OccupyTable(visitorName string) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-
-	for r.tables == 0 {
-		fmt.Println("ждёт свободный столик", visitorName)
-		r.cond.Wait()
+func NewRestaurant(maxWight int) *Restaurant{
+	return &Restaurant{
+		table: make([]int, 0, maxWight),
+		maxTables: maxWight,
 	}
-
-	r.tables--
-	fmt.Println("сел за столик.", visitorName, "Свободно: ", r.tables)
 }
 
-func (r *Restaurant) ReleaseTable(visitorName string) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
+func (res *Restaurant) Bronirovanie(wg *sync.WaitGroup){
+	defer wg.Done()  // сообщаем, что горутина завершилась
+    
+	for i := 1; i <= res.maxTables; i++ {
+		res.mu.Lock()
+		if len(res.table) < res.maxTables {
+			res.table = append(res.table, i)
+			fmt.Printf("Столик %d заняли. Свободно: %d\n", i, res.maxTables - len(res.table))
+		} else {
+			fmt.Println("Все столики уже заняты!")
+			res.mu.Unlock()
+			break
+		}
+		
+		res.mu.Unlock()
+		// Имитация времени на обслуживание
+		time.Sleep(100 * time.Millisecond)
+  }
+}
 
-	// Освобождаем столик
-	r.tables++
-	fmt.Println("освободил столик: ", visitorName, "Свободно: ", r.tables)
 
-	// Будим ОДНОГО ждущего (того, кто первым встал в очередь)
-	r.cond.Signal()
+func main(){
+	var wg sync.WaitGroup
+    
+  res := NewRestaurant(5)
+    
+  wg.Add(1)
+  go res.Bronirovanie(&wg)
+    
+  fmt.Println("Начало бронирования")
+  wg.Wait()  // ждем завершения горутины
+  fmt.Println("Бронирование завершено")
 }
