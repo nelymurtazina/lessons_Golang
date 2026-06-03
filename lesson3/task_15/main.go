622d94f8cb6b32c2dfa869142b5a0ced9cbb0a9a
Nely <nelymurtazina@gmail.com>
2026-06-03 11:39:26 +0300

    feat: update task_15

--- lesson3/task_15/main.go
+++ lesson3/task_15/main.go
@@ -6,37 +6,47 @@ import (
 	"time"
 )
 
-type Connection struct {
+type Connection struct{
 	ID int
 }
 
-type Database struct {
-	conn *Connection
+type DataBase struct{
+	conn *Connection 
 	once sync.Once
 }
 
-func (db *Database) GetConnection() *Connection {
+func (db *DataBase) GetConnection() *Connection{
+	//выполнится ТОЛЬКО ОДИН РАЗ
 	db.once.Do(func() {
-		fmt.Println("Инициализация подключения к БД")
-		time.Sleep(100 * time.Millisecond)
+		fmt.Println("Подключение к БД")
+		time.Sleep(1 * time.Second)
 		db.conn = &Connection{ID: 1}
-		fmt.Println("Подключение создано!")
+		fmt.Println("Подключение создано")
 	})
+	//если нет - создает, сохраняет и возвращает подключение 
+	//если есть - возвращают подключение (не создают заново)
 	return db.conn
 }
 
-func main() {
-	db := &Database{}
+func main(){
+	db := &DataBase{}
+
 	var wg sync.WaitGroup
 
-	for i := 0; i < 10; i++ {
+	countGo := 10
+
+	for i := 0; i<countGo;i++{
 		wg.Add(1)
-		go func(id int) {
+		go func (id int) {
 			defer wg.Done()
 			conn := db.GetConnection()
-			fmt.Println("Горутина", id, "получила подключение", conn.ID)
+			fmt.Println("Подключение: ", conn.ID)
 		}(i)
 	}
 
 	wg.Wait()
+
+	fmt.Println("Тест, повторный вызов")
+	conn := db.GetConnection()
+	fmt.Println("Подключение: ", conn.ID)
 }
